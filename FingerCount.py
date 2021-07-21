ec7f0d864036c04190f55192c6b29ddef45a4578
Abhimanyu Saxena <33328513+abhimanyu-saxena@users.noreply.github.com>
2021-07-21 19:46:16 +0530

    Add files via upload

--- FingerCount.py
+++ FingerCount.py
@@ -18,6 +18,27 @@ cap = cv2.VideoCapture(1)
 cap.set(3, wcam)
 cap.set(4, hcam)
 det = track.handDetector()
+
+
+def numFingers(landmarklist):
+    tiplist =[8,12,16,20]
+    status=[]
+    if landmarklist[4][1]<landmarklist[5][1]:
+        cv2.putText(img,"open", (landmarklist[4][1],landmarklist[4][2]), cv2.FONT_HERSHEY_PLAIN, 1, (0,0,0), 2)
+        status.append(1)
+    else:
+        cv2.putText(img,"close", (landmarklist[4][1],landmarklist[4][2]), cv2.FONT_HERSHEY_PLAIN, 1, (0,0,0), 2)
+        status.append(0)
+    for i in tiplist:
+        if lmslist[i][2]<lmslist[i-2][2]:
+            cv2.putText(img,"open", (landmarklist[i][1],landmarklist[i][2]), cv2.FONT_HERSHEY_PLAIN, 1, (0,0,0), 2)
+            status.append(1)
+        else:
+            cv2.putText(img,"close", (landmarklist[i][1],landmarklist[i][2]), cv2.FONT_HERSHEY_PLAIN, 1, (0,0,0), 2)
+            status.append(0)
+    return status
+    
+    
 while True:
     _,img=cap.read()
     img=cv2.flip(img,1)
@@ -27,49 +48,12 @@ while True:
     fps = int(1/(currT-prevT))
     prevT = currT
             
-    cv2.putText(img,str(fps), (10,70), cv2.FONT_HERSHEY_PLAIN, 2, (0,255,0), 2)
+    cv2.putText(img,"FPS:"+str(fps), (10,70), cv2.FONT_HERSHEY_PLAIN, 2, (0,255,0), 2)
     if len(lmslist)!=0:
-        status=[]
-        
-        #thumb
-        if lmslist[4][1]<lmslist[5][1]:
-            cv2.putText(img,"open", (lmslist[4][1],lmslist[4][2]), cv2.FONT_HERSHEY_PLAIN, 1, (0,0,0), 2)
-            status.append(1)
-        else:
-            cv2.putText(img,"close", (lmslist[4][1],lmslist[4][2]), cv2.FONT_HERSHEY_PLAIN, 1, (0,0,0), 2)
-            status.append(0)
-        #index finger
-        if lmslist[8][2]<lmslist[6][2]:
-            cv2.putText(img,"open", (lmslist[8][1],lmslist[8][2]), cv2.FONT_HERSHEY_PLAIN, 1, (0,0,0), 2)
-            status.append(1)
-        else:
-            cv2.putText(img,"close", (lmslist[8][1],lmslist[8][2]), cv2.FONT_HERSHEY_PLAIN, 1, (0,0,0), 2)
-            status.append(0)
-        #middle finger
-        if lmslist[12][2]<lmslist[10][2]:
-            cv2.putText(img,"open", (lmslist[12][1],lmslist[12][2]), cv2.FONT_HERSHEY_PLAIN, 1, (0,0,0), 2)
-            status.append(1)
-        else:
-            cv2.putText(img,"close", (lmslist[12][1],lmslist[12][2]), cv2.FONT_HERSHEY_PLAIN, 1, (0,0,0), 2)
-            status.append(0)
-        #ring finger
-        if lmslist[16][2]<lmslist[14][2]:
-            cv2.putText(img,"open", (lmslist[16][1],lmslist[16][2]), cv2.FONT_HERSHEY_PLAIN, 1, (0,0,0), 2)
-            status.append(1)
-        else:
-            cv2.putText(img,"close", (lmslist[16][1],lmslist[16][2]), cv2.FONT_HERSHEY_PLAIN, 1, (0,0,0), 2)
-            status.append(0)
-        #little finger
-        if lmslist[20][2]<lmslist[18][2]:
-            cv2.putText(img,"open", (lmslist[20][1],lmslist[20][2]), cv2.FONT_HERSHEY_PLAIN, 1, (0,0,0), 2)
-            status.append(1)
-        else:
-            cv2.putText(img,"close", (lmslist[20][1],lmslist[20][2]), cv2.FONT_HERSHEY_PLAIN, 1, (0,0,0), 2)
-            status.append(0)
-        
-#        cv2.putText(img,"iTip", (lmslist[8][1],lmslist[8][2]), 
-#        cv2.FONT_HERSHEY_PLAIN, 1, (0,0,0), 2)
-        print(status)
+        status=numFingers(lmslist)
+        #print(status)
+        totalFin = sum(status)
+        cv2.putText(img,str(totalFin), (600,70), cv2.FONT_HERSHEY_PLAIN, 2, (0,255,0), 2)
     cv2.imshow("Frame", img)
     #cv2.waitKey(1)
     if cv2.waitKey(1) == ord('q'):
